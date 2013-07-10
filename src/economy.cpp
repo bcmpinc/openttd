bc32bd80356f0f10010a904d2c410123ab9b7152
Bauke Conijn <bcmpinc@users.sourceforge.net>
2013-07-10 17:32:39 +0200

    Made transfer payments optional.

--- src/economy.cpp
+++ src/economy.cpp
@@ -1180,15 +1180,24 @@ void CargoPayment::PayFinalDelivery(const CargoPacket *cp, uint count)
 		this->owner = Company::Get(this->front->owner);
 	}
 
-	/* Handle end of route payment */
-	DeliverGoods(count, this->ct, this->current_station, cp->SourceStationXY(), cp->DaysInTransit(), this->owner, cp->SourceSubsidyType(), cp->SourceSubsidyID()); //Required to actually deliver the goods
-	
-	
-	Money profit = GetTransportedGoodsIncome(count,DistanceManhattan(cp->LoadedAtXY(), Station::Get(this->current_station)->xy),cp->DaysInTransit(),this->ct);  //Allows the actual profits for only the last part of the trip to be calculated and, consequently, paid.
+	if (_settings_game.economy.transfer_payments) {
+		/* Handle end of route payment */
+		//Required to actually deliver the goods:
+		DeliverGoods(count, this->ct, this->current_station, cp->SourceStationXY(), cp->DaysInTransit(), this->owner, cp->SourceSubsidyType(), cp->SourceSubsidyID()); 
+		//Allows the actual profits for only the last part of the trip to be calculated and, consequently, paid:
+		Money profit = GetTransportedGoodsIncome(count,DistanceManhattan(cp->LoadedAtXY(), Station::Get(this->current_station)->xy),cp->DaysInTransit(),this->ct);  
 
-	/* The vehicle's profit is whatever route profit there is minus feeder shares. */
-	this->visual_profit += profit;
-	this->route_profit += profit;
+		/* The vehicle's profit is whatever route profit there is minus feeder shares. */
+		this->visual_profit += profit;
+		this->route_profit += profit;
+	} else {
+		 /* Handle end of route payment */
+		 Money profit = DeliverGoods(count, this->ct, this->current_station, cp->SourceStationXY(), cp->DaysInTransit(), this->owner, cp->SourceSubsidyType(), cp->SourceSubsidyID());
+		 this->route_profit += profit;
+   
+		 /* The vehicle's profit is whatever route profit there is minus feeder shares. */
+		 this->visual_profit += profit - cp->FeederShare(count);
+	}
 }
 
 /**
@@ -1206,10 +1215,15 @@ Money CargoPayment::PayTransfer(CargoPacket *cp, uint count)
 			cp->DaysInTransit(),
 			this->ct);
 	
-	cp->ResetTransitDays();		//Reset the amount of days this package has been in transit, important for the next leg
-
+	if (_settings_game.economy.transfer_payments) {
+		//Reset the amount of days this package has been in transit, important for the next leg
+		cp->ResetTransitDays();
+		this->route_profit += profit;
+	} else {
+		profit = profit * _settings_game.economy.feeder_payment_share / 100;
+	}
+	
 	this->visual_transfer += profit; // accumulate transfer profits for whole vehicle
-	this->route_profit += profit;
 	return profit; // account for the (virtual) profit already made for the cargo packet
 }
 
