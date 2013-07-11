df2135d525de610498dfbe38522cfbcd13dc86dd
Bauke Conijn <bcmpinc@users.sourceforge.net>
2013-07-11 17:03:56 +0200

    Version bump (191)

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -89,7 +89,7 @@ enum SLRefType {
 
 /** Highest possible savegame version. */
 #define SL_MAX_VERSION 255
-#define SL_TRANSFER_PAYMENTS 190
+#define SL_TRANSFER_PAYMENTS 191
 
 /** Flags of a chunk. */
 enum ChunkType {
